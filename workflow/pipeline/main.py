4792e75b4953611f4743da8ce9754a956b3dd236
Danilo <danilo.ren12@gmail.com>
2024-04-26 20:53:35 -0300

    download func to fix

--- workflow/pipeline/main.py
+++ workflow/pipeline/main.py
@@ -1,11 +1,42 @@
 import kfp
 from kfp import dsl
 
-@dsl.component
+@dsl.component(packages_to_install=['Minio','cryptography','Werkzeug'])
 def download_data(url: str, filename: str) -> str:
     from urllib.request import urlretrieve
+    from werkzeug.utils import secure_filename
+    from minio import Minio
+    import os
+    def upload_file(path):
+        with open(path) as f: data = f.read()
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if path == "":
+            return "file not found"
+        if path:
+            filename = secure_filename(path)
+            size = os.stat(path).st_size
+            msg = upload_object(filename, data, size)
+            return msg
+    def upload_object(filename, data, length):
+        client = Minio(MINIO_ENDPOINT, MINIO_USER, MINIO_PASS, secure=False)
+        # Make bucket if not exist.
+        found = client.bucket_exists(BUCKET_NAME)
+        if not found:
+            client.make_bucket(BUCKET_NAME)
+        else:
+            return f"Bucket {BUCKET_NAME} already exists"
+        client.put_object(BUCKET_NAME, filename, data, length)
+        return f"{filename} is successfully uploaded to bucket {BUCKET_NAME}."
+    # download file
     path, headers = urlretrieve(url, filename)
-    return path
+    # minio credentials
+    MINIO_USER = "minioadmin"
+    MINIO_PASS = "minioadmin"
+    BUCKET_NAME = "flask-minio"
+    MINIO_ENDPOINT = "minio-svc.minio:9000"
+    msg = upload_file(path)
+    return msg
 
 @dsl.pipeline
 def my_pipeline(url: str, filename: str) -> str:
