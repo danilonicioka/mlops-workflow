053952c00b7e4ef7703f892ba2410e84195aaea3
Danilo Nicioka <danilo.ren12@gmail.com>
2024-04-27 14:54:05 -0300

    update main with test func

--- workflow/pipeline/main.py
+++ workflow/pipeline/main.py
@@ -6,7 +6,9 @@ def download_data(url: str, filename: str) -> str:
     from urllib.request import urlretrieve
     from werkzeug.utils import secure_filename
     from minio import Minio
+    from io import BytesIO
     import os
+
     def upload_file(path):
         source_file = path
         # If the user does not select a file, the browser submits an
@@ -15,10 +17,14 @@ def download_data(url: str, filename: str) -> str:
             return "file not found"
         if source_file:
             destination_file = secure_filename(source_file)
-            size = os.stat(path).st_size
-            msg = upload_object(destination_file, source_file, size)
+            # size = os.stat(path).st_size
+            msg = upload_object(destination_file, source_file)#, size)
             return msg
-    def upload_object(destination_file, source_file, size):
+        
+    def upload_object(destination_file, source_file):#, size):
+        with open(source_file) as f: data = f.read()
+        data_bytes = data.encode('utf-8')
+        data_bytesio = BytesIO(data_bytes)
         bucket_name = BUCKET_NAME
         # class Dataset:
         #     def __init__(self, data=None):
@@ -34,7 +40,7 @@ def download_data(url: str, filename: str) -> str:
         else:
             print("Bucket", bucket_name, "already exists")
         # client.put_object(bucket_name, destination_file, source_file, size)
-        client.fput_object(bucket_name, destination_file, source_file, size)
+        client.put_object(bucket_name, destination_file, data_bytesio, length=len(data_bytes), content_type='application/csv')
         return f"{destination_file} is successfully uploaded to bucket {bucket_name}."
     # download file
     path, headers = urlretrieve(url, filename)
@@ -44,6 +50,7 @@ def download_data(url: str, filename: str) -> str:
     BUCKET_NAME = "flask-minio"
     MINIO_ENDPOINT = "minio-svc.minio:9000"
     msg = upload_file(path)
+    #print(msg)
     return msg
 
 @dsl.pipeline
