8facc7dc7df8ad93472bbc61c7c908463708ea0d
Danilo Nicioka <danilo.ren12@gmail.com>
2024-05-31 16:55:12 -0300

    test with experiment and run pipeline based on package

--- workflow/pipeline/main.py
+++ workflow/pipeline/main.py
@@ -253,7 +253,7 @@ client = kfp.Client(host=KFP_HOST)  # Use the configured KFP host
 # Create an experiment
 experiment = client.create_experiment(name='experiment', namespace='kubeflow')
 
-client.run_pipeline(experiment.id, 'mlops_pipeline', pipeline_filename, enable_caching=False,
+client.run_pipeline(pipeline_filename, enable_caching=False,
     params={
         'repo_url': REPO_URL,
         'cloned_dir': CLONED_DIR,
