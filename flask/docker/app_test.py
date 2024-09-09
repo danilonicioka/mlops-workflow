81f80c07ea5bc64fb03af2626755a98ef55ec1a9
Danilo Nicioka <danilo.ren12@gmail.com>
2024-09-09 19:47:59 -0300

    fix accuracy path

--- flask/docker/app_test.py
+++ flask/docker/app_test.py
@@ -97,7 +97,7 @@ TEMP_FILE_N_SAMPLES_SINCE_LAST_RUN = "n_samples_since_last_run"
 TEMP_FILE_N_SAMPLES_IN_LAST_RUN = "n_samples_in_last_run"
 PERFORMANCE_FACTOR = 5.0
 TEMP_FILE_ACC_IN_LAST_RUN = "accuracy_in_last_run"
-LAST_ACC_OBJECT_NAME = "last_acc"
+LAST_ACC_OBJECT_NAME = "accuracy-score/last_acc.txt"
 
 ####### Class to access kubeflow from outside the cluster
 
