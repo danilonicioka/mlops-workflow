3d85013559b30f717652d0a9fd1ab120d82192a2
danilonicioka <danilo.ren12@gmail.com>
2024-09-11 19:34:52 +0000

    test dsl if block

--- flask/docker/app_test.py
+++ flask/docker/app_test.py
@@ -91,6 +91,7 @@ COMMIT_MSG_APPEND = 'Update .dvc file'
 
 TRIGGER_TYPE = '0'
 QUANTITY_FACTOR = 0.1
+FIRST_RUN = '0'
 
 TEMP_DIR = "tmp"
 TEMP_FILE_N_SAMPLES_SINCE_LAST_RUN = "n_samples_since_last_run.txt"
@@ -843,7 +844,7 @@ def append_csv():
             'object_name': OBJECT_NAME,
             'svc_acc': SVC_ACC,
             'trigger_type': trigger_type,  # Include the trigger type in the pipeline parameters
-            'first_run': False,
+            'first_run': FIRST_RUN,
             'performance_factor': performance_factor,
             'last_accuracy_object_name': LAST_ACC_OBJECT_NAME,
             'tmp_dir': TEMP_DIR,
