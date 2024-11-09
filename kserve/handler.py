546ef8a5785950dd36eb37c3216512e6be78aeb8
Danilo <danilo.ren12@gmail.com>
2024-11-09 14:40:50 -0300

    test new handler

--- kserve/handler.py
+++ kserve/handler.py
@@ -1,26 +1,101 @@
 from ts.torch_handler.base_handler import BaseHandler
 import torch
+import os
+from torch import nn
 
 class ModelHandler(BaseHandler):
-    def initialize(self, context):
-        # Load the model definition from model.py and set the weights from model.pt
-        model_dir = context.system_properties.get("model_dir")
-        serialized_file = context.system_properties.get("serialized_file")
+    def __init__(self):
+        self._context = None
+        self.initialized = False
+        self.explain = False
+        self.target = 0
 
-        # Load model architecture and weights
-        self.model = torch.jit.load(f"{model_dir}/{serialized_file}")
-        self.model.eval()
+    def initialize(self, context):
+        """
+        Initialize model. This will be called during model loading time
+        :param context: Initial context contains model server system properties.
+        :return:
+        """
+        self._context = context
+
+        #  load the model
+        self.manifest = context.manifest
+
+        properties = context.system_properties
+        model_dir = properties.get("model_dir")
+        self.device = torch.device("cuda:" + str(properties.get("gpu_id")) if torch.cuda.is_available() else "cpu")
+
+        # Read model serialize/pt file
+        serialized_file = self.manifest['model']['serializedFile']
+        model_pt_path = os.path.join(model_dir, serialized_file)
+        if not os.path.isfile(model_pt_path):
+            raise RuntimeError("Missing the model.pt file")
+
+        # Build model with non-linear activation function
+        class InterruptionModel(nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.layer_1 = nn.Linear(in_features=29, out_features=200)
+                self.layer_2 = nn.Linear(in_features=200, out_features=100)
+                self.layer_3 = nn.Linear(in_features=100, out_features=1)
+                self.relu = nn.ReLU() # <- add in ReLU activation function
+                # Can also put sigmoid in the model
+                # This would mean you don't need to use it on the predictions
+                # self.sigmoid = nn.Sigmoid()
+
+            def forward(self, x):
+                # Intersperse the ReLU activation function between layers
+                return self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(x)))))
+            
+        model = InterruptionModel()
+
+        self.model = model.load_state_dict(torch.load(model_pt_path, weights_only=True))
+
         self.initialized = True
 
     def preprocess(self, data):
-        # Preprocess logic
-        data = data[0].get("data") or data[0].get("body")
-        tensor_data = torch.tensor(data["instances"], dtype=torch.float32)
+        """
+        Transform raw input into model input data.
+        :param batch: list of raw requests, should match batch size
+        :return: list of preprocessed model input data
+        """
+        # Take the input data and make it inference ready
+        preprocessed_data = data[0].get("data")
+        if preprocessed_data is None:
+            preprocessed_data = data[0].get("body")
+
+        tensor_data = torch.tensor(preprocessed_data["instances"], dtype=torch.float32)
+
         return tensor_data
 
     def inference(self, model_input):
+        """
+        Internal inference methods
+        :param model_input: transformed model input data
+        :return: list of inference output in NDArray
+        """
+        # Do some inference call to engine here and return output
         with torch.no_grad():
             return self.model(model_input)
 
     def postprocess(self, inference_output):
-        return inference_output.tolist()
+        """
+        Return inference result.
+        :param inference_output: list of inference output
+        :return: list of predict results
+        """
+        # Take output from network and post-process to desired format
+        postprocess_output = inference_output
+        return postprocess_output.tolist()
+
+    def handle(self, data, context):
+        """
+        Invoke by TorchServe for prediction request.
+        Do pre-processing of data, prediction using model and postprocessing of prediciton output
+        :param data: Input data for prediction
+        :param context: Initial context contains model server system properties.
+        :return: prediction output
+        """
+        model_input = self.preprocess(data)
+        model_output = self.inference(model_input)
+        return self.postprocess(model_output)
